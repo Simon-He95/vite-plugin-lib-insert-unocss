c2d3d10d408741957e3d8a113ec64c054bf36767
Simon-He95 <674949287@qq.com>
2022-09-18 18:46:37 +0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import fs from 'fs'
+export function vitePluginLibInsertUnocss() {
+  return {
+    name: 'vite-plugin-lib-insert-unocss',
+    writeBundle(options: any, bundle: any) {
+      const name = options.entryFileNames
+      const dirname = `${options.dir}/${name}`
+      const { code } = bundle[name]
+      fs.writeFile(dirname, `// @unocss-include
+${code}`, (err: any) => {
+        if (err)
+          throw err
+      })
+    },
+  }
+}
